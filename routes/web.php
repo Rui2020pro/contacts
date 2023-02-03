f3925ea72f57f3eceb738a0bbb130d0548e7d6d4
Rui2020pro <matosrui1998@gmail.com>
2023-02-03 12:07:31 +0000

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacts', [ContactsController::class, 'index'])->name('contacts.index');
